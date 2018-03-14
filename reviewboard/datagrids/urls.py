a7274d6e46876671bd3df3571efcbe8717847c00
makhalin <makhalin@yandex-team.ru>
2018-03-14 18:38:44 +0500

    fix

--- reviewboard/datagrids/urls.py
+++ reviewboard/datagrids/urls.py
@@ -13,16 +13,16 @@ urlpatterns = patterns(
     url(r'^dashboard/$', 'dashboard', name='dashboard'),
 
 #     # Users
-#     url(r'^users/$', 'users_list', name='all-users'),
-#     url(r"^users/(?P<username>[A-Za-z0-9@_\-\.'\+]+)/$",
-#         'submitter', name='user'),
-#     url(r"^users/(?P<username>[A-Za-z0-9@_\-\.'\+]+)/(?P<grid>[a-z-]+)/$",
-#         'submitter', name='user-grid'),
+    url(r'^users/$', 'users_list', name='all-users'),
+    url(r"^users/(?P<username>[A-Za-z0-9@_\-\.'\+]+)/$",
+        'submitter', name='user'),
+    url(r"^users/(?P<username>[A-Za-z0-9@_\-\.'\+]+)/(?P<grid>[a-z-]+)/$",
+        'submitter', name='user-grid'),
 #
 #
 #     # Groups
-#     url(r'^groups/$', 'group_list', name='all-groups'),
-#     url(r'^groups/(?P<name>[A-Za-z0-9_-]+)/$', 'group', name='group'),
-#     url(r'^groups/(?P<name>[A-Za-z0-9_-]+)/members/$',
-#         'group_members', name='group-members'),
+    url(r'^groups/$', 'group_list', name='all-groups'),
+    url(r'^groups/(?P<name>[A-Za-z0-9_-]+)/$', 'group', name='group'),
+    url(r'^groups/(?P<name>[A-Za-z0-9_-]+)/members/$',
+        'group_members', name='group-members'),
 )
